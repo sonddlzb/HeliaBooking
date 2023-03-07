8ac85e0be270d98643e2f1ea843a0e120d3583ed
sondd <sonddlzb@gmail.com>
2023-03-07 09:11:11 +0700

    TASK #16225: implement signing in with google

--- HeliaHotelBooking/UI/AcountSetup/SignInViewController.swift
+++ HeliaHotelBooking/UI/AcountSetup/SignInViewController.swift
@@ -18,21 +18,30 @@ class SignInViewController: UIViewController {
     @IBAction func popScreen(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
-    
+
     @IBAction func facebookLoginAction(_ sender: Any) {
         FacebookSignInManager.shared.delegate = self
         FacebookSignInManager.shared.signIn(withPresenting: self)
     }
-    
+
     @IBAction func signInWithPasswordAction(_ sender: Any) {
     }
     
-    
+    @IBAction func didTapSignInWithGoogle(_ sender: TapableView) {
+        GoogleSignInManager.shared.delegate = self
+        GoogleSignInManager.shared.signIn(withPresenting: self)
+    }
 }
 
 extension SignInViewController: FacebookSignInDelegate {
     func facebookSignInManagerDidSignInSuccessfully(_ facebookSignInManager: FacebookSignInManager) {
         //return home
         print("successfully")
+}
+
+// MARK: - GoogleSignInManagerDelegate
+extension SignInViewController: GoogleSignInManagerDelegate {
+    func googleSignInManagerDidSignInSuccessfully(_ googleSignInManager: GoogleSignInManager) {
+        // route to Home
     }
 }
